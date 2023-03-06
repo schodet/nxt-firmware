f013e6c23adb9d068f1ee5a372f9454e5db09797
Nicolas Schodet <nico@ni.fr.eu.org>
2023-03-06 17:11:21 +0100

    Remove unused IAR only files
    Building with IAR is no longer supported.

--- AT91SAM7S256/SAM7S256/Include/Dlib_Product.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DLIB_PRODUCTS_H_
-#define _DLIB_PRODUCTS_H_
-
-/* Nothing needed here */
-
-#endif
-
-
--- AT91SAM7S256/SAM7S256/Lib/dl4tptinl8n.h
+++
@@ -1,9 +0,0 @@
-/* Customer-specific DLib configuration. */
-/* Copyright (C) 2003 IAR Systems.  All rights reserved. */
-
-#ifndef _DLIB_CONFIG_H
-#define _DLIB_CONFIG_H
-
-/* No changes to the defaults. */
-
-#endif /* _DLIB_CONFIG_H */
